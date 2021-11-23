2b12837a94f0374c9a94409fbbcf0c5484350d24
jwon42 <jwon@student.42seoul.kr>
2021-11-23 15:52:31 +0900

    feat: front_setting(add route setting, add default pages component)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
+import Interview from './pages/Interview';
+import Random from './pages/Random';
+import Mypage from './pages/Mypage';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/interview" element={<Interview />} />
+        <Route path="/random" element={<Random />} />
+        <Route path="/mypage" element={<Mypage />} />
+        <Route path="/mypage/:userID" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/pages/Mypage/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Random: React.FC = () => {
+  return <div>Mypage Page</div>;
+};
+
+export default Random;
